84236979e8547651985f7f799b93dde9e53b22c7
nathanjassi <nathan.k.jassi@gmail.com>
2019-07-19 00:00:02 +0100

    Redesigned index page. Added color hover functionality & toggle switch.

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,10 +5,16 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+const colorizeText = document.querySelectorAll('.colorize-text');
+const colorBtn = document.querySelector('.color-btn');
+
 // Set the initial state of menu
 let showMenu = false;
+let enableColors = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+colorBtn.addEventListener('click', () => setTextColor(colorizeText));
+setTextColor(colorizeText);
 
 function toggleMenu() {
   if (!showMenu) {
@@ -29,5 +35,41 @@ function toggleMenu() {
 
     // Set menu state
     showMenu = false;
+  }
+}
+
+// set the color of each letter
+// sets upto 5 colors
+function setTextColor(text) {
+  if (!enableColors) {
+    for (let i = 0; i < text.length; i++) {
+      const string = text[i].innerText;
+      text[i].innerHTML = '';
+      let colorNumber = -1;
+      for (let x = 0; x < string.length; x++) {
+        const span = document.createElement('span');
+        text[i].appendChild(span);
+        span.innerText = string[x];
+        colorNumber = colorNumber + 1 < 5 ? colorNumber + 1 : 0;
+        span.classList.add(`color-${colorNumber + 1}`);
+      }
+    }
+    enableColors = true;
+    console.log('enable color');
+  } else {
+    for (let i = 0; i < text.length; i++) {
+      const string = text[i].innerText;
+      text[i].innerHTML = '';
+      let colorNumber = -1;
+      for (let x = 0; x < string.length; x++) {
+        const span = document.createElement('span');
+        text[i].appendChild(span);
+        span.innerText = string[x];
+        colorNumber = colorNumber + 1 < 5 ? colorNumber + 1 : 0;
+        span.classList.remove(`color-${colorNumber + 1}`);
+      }
+    }
+    enableColors = false;
+    console.log('disable color');
   }
 }
