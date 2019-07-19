bfc7a45c7fdcf7de4e241c2f9bed18d7b2140293
nathanjassi <nathan.k.jassi@gmail.com>
2019-07-19 20:50:24 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -5,16 +5,13 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-const colorizeText = document.querySelectorAll('.colorize-text');
-const colorBtn = document.querySelector('.color-btn');
+const colorizerText = document.querySelectorAll('.colorizer-text');
 
 // Set the initial state of menu
 let showMenu = false;
-let enableColors = false;
 
 menuBtn.addEventListener('click', toggleMenu);
-colorBtn.addEventListener('click', () => setTextColor(colorizeText));
-setTextColor(colorizeText);
+setTextColor(colorizerText);
 
 function toggleMenu() {
   if (!showMenu) {
@@ -41,35 +38,16 @@ function toggleMenu() {
 // set the color of each letter
 // sets upto 5 colors
 function setTextColor(text) {
-  if (!enableColors) {
-    for (let i = 0; i < text.length; i++) {
-      const string = text[i].innerText;
-      text[i].innerHTML = '';
-      let colorNumber = -1;
-      for (let x = 0; x < string.length; x++) {
-        const span = document.createElement('span');
-        text[i].appendChild(span);
-        span.innerText = string[x];
-        colorNumber = colorNumber + 1 < 5 ? colorNumber + 1 : 0;
-        span.classList.add(`color-${colorNumber + 1}`);
-      }
+  for (let i = 0; i < text.length; i++) {
+    const string = text[i].innerText;
+    text[i].innerHTML = '';
+    let colorNumber = -1;
+    for (let x = 0; x < string.length; x++) {
+      const span = document.createElement('span');
+      text[i].appendChild(span);
+      span.innerText = string[x];
+      colorNumber = colorNumber + 1 < 5 ? colorNumber + 1 : 0;
+      span.classList.add(`color-${colorNumber + 1}`);
     }
-    enableColors = true;
-    console.log('enable color');
-  } else {
-    for (let i = 0; i < text.length; i++) {
-      const string = text[i].innerText;
-      text[i].innerHTML = '';
-      let colorNumber = -1;
-      for (let x = 0; x < string.length; x++) {
-        const span = document.createElement('span');
-        text[i].appendChild(span);
-        span.innerText = string[x];
-        colorNumber = colorNumber + 1 < 5 ? colorNumber + 1 : 0;
-        span.classList.remove(`color-${colorNumber + 1}`);
-      }
-    }
-    enableColors = false;
-    console.log('disable color');
   }
 }
