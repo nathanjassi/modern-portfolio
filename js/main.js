adcc3eb19b836e31a53e13394d388c4f478c8a15
nathanjassi <nathan.k.jassi@gmail.com>
2019-07-20 19:13:56 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -7,33 +7,84 @@ const navItems = document.querySelectorAll('.nav-item');
 
 const colorizerText = document.querySelectorAll('.colorizer-text');
 
+const lockTextColor = document.querySelector('.fa-lightbulb');
+// Set initial state of text color
+let colorText = false;
+
 // Set the initial state of menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+lockTextColor.addEventListener('click', toggleTextColor);
+// menuBtn.addEventListener('click', toggleMenu);
 setTextColor(colorizerText);
 
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach((item) => item.classList.add('show'));
-
-    // Set menu state
-    showMenu = true;
+function toggleTextColor() {
+  const colorizerColor1 = document.querySelectorAll('.color-1');
+  const colorizerColor2 = document.querySelectorAll('.color-2');
+  const colorizerColor3 = document.querySelectorAll('.color-3');
+  const colorizerColor4 = document.querySelectorAll('.color-4');
+  const colorizerColor5 = document.querySelectorAll('.color-5');
+  if (!colorText) {
+    console.log(colorizerColor1.length);
+    for (i = 0; i < colorizerColor1.length; i++) {
+      colorizerColor1[i].style.color = '#d1495b';
+    }
+    for (i = 0; i < colorizerColor2.length; i++) {
+      colorizerColor2[i].style.color = '#facc6b';
+    }
+    for (i = 0; i < colorizerColor3.length; i++) {
+      colorizerColor3[i].style.color = '#084c61';
+    }
+    for (i = 0; i < colorizerColor4.length; i++) {
+      colorizerColor4[i].style.color = '#1b998b';
+    }
+    for (i = 0; i < colorizerColor5.length; i++) {
+      colorizerColor5[i].style.color = '#00798c';
+    }
+    lockTextColor.classList.toggle('fas');
+    colorText = true;
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach((item) => item.classList.remove('show'));
-
-    // Set menu state
-    showMenu = false;
+    for (i = 0; i < colorizerColor1.length; i++) {
+      colorizerColor1[i].removeAttribute('style');
+    }
+    for (i = 0; i < colorizerColor2.length; i++) {
+      colorizerColor2[i].removeAttribute('style');
+    }
+    for (i = 0; i < colorizerColor3.length; i++) {
+      colorizerColor3[i].removeAttribute('style');
+    }
+    for (i = 0; i < colorizerColor4.length; i++) {
+      colorizerColor4[i].removeAttribute('style');
+    }
+    for (i = 0; i < colorizerColor5.length; i++) {
+      colorizerColor5[i].removeAttribute('style');
+    }
+    lockTextColor.classList.toggle('fas');
+    colorText = false;
   }
 }
+
+// function toggleMenu() {
+//   if (!showMenu) {
+//     menuBtn.classList.add('close');
+//     menu.classList.add('show');
+//     menuNav.classList.add('show');
+//     menuBranding.classList.add('show');
+//     navItems.forEach((item) => item.classList.add('show'));
+
+//     // Set menu state
+//     showMenu = true;
+//   } else {
+//     menuBtn.classList.remove('close');
+//     menu.classList.remove('show');
+//     menuNav.classList.remove('show');
+//     menuBranding.classList.remove('show');
+//     navItems.forEach((item) => item.classList.remove('show'));
+
+//     // Set menu state
+//     showMenu = false;
+//   }
+// }
 
 // set the color of each letter
 // sets upto 5 colors
